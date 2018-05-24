59ec232912b9752cb5ab26f27d83e120f4e64f07
Greifrut <artur.bunko@gmail.com>
2018-05-24 18:46:59 +0300

    card draggable

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,28 @@
 <template>
-  <div id="app" class="row">
+  <draggable v-model="lists" :options="{group: 'lists'}" class="row dragArea" @end="listMoved">
     <div v-for="(list, index) in original_lists" class="col-3">
       <h6>{{list.name}}</h6>
       <hr />
-      <div v-for="(card, index) in list.cards" class="card card-body">
-        {{card.name}}
-      </div>
+      <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
+        <div v-for="(card, index) in list.cards" class="card card-body">
+          {{card.name}}
+        </div>
+      </draggable>
       <div class="card car-body">
         <textarea  v-model="messages[list.id]" class="form-control"></textarea>
         <button v-on:click="submitMessages(list.id)" class="btn btn-primary">Add</button>
       </div>
     </div>
-  </div>
+  </draggable>
 </template>
 
 <script>
+import draggable from "vuedraggable"
 export default {
+  components: {draggable},
+
   props: ["original_lists"],
+
   data: function() {
     return {
       messages: {},
@@ -24,6 +30,38 @@ export default {
     }
   },
   methods: {
+    cardMoved: function(event) {
+      if (event.added == undefined) {return}
+
+      const list_index = this.lists.findIndex((list)=> {
+        return list.cards.find((card) => {
+          return card.id === event.added.element.id
+        })
+      })
+      var data = new FormData
+      data.append("card[list_id", this.lists[list_index].id)
+      data.append("card[position]", event.newIndex + 1)
+
+      Rails.ajax({
+        url: `/cards/${event.added.element.id}/move`,
+        type: "PATCH",
+        data: data,
+        dataType: "json"
+      })
+    },
+
+    listMoved: function(event) {
+      var data = new FormData
+      data.append("list[position]", event.newIndex + 1)
+
+      Rails.ajax({
+        url: `/lists/${this.lists[event.newIndex].id}/move`,
+        type: 'PATCH',
+        data: data,
+        dataType: 'json',
+      })
+    },
+
     submitMessages: function(list_id) {
 
       var data = new FormData
@@ -50,5 +88,8 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+}
+.dragArea {
+  min-height: 20px;
 }
 </style>
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :lists
+  resources :lists do
+    member do
+      patch :move
+    end
+  end
+  resources :cards do
+    member do
+      patch :move
+    end
+  end
 
   root to: "lists#index"
 end
